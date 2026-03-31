8f51a491c79f9f12040a86308dc06a897689c810
Pritam Patil <pritam7096p@gmail.com>
2026-03-31 06:03:12 +0000

    datatypes

---
+++ .devcontainer/basics/dataTypes2.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version
+
+console.log(3+3) //readability    op => 6
+console.log("pritam")   // op => pritam
+
+let name = "pritam"
+let age = 20
+let isLoggedIn = false
+let state;
+
+console.log(typeof "pritam")  //string
+
+console.log(typeof undefined)   //undefined
+console.log(typeof null)   //object
