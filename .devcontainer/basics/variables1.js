433fa31f0e5e30c1d77b46a62b40a1f9e0d8ab09
Pritam Patil <pritam7096p@gmail.com>
2026-03-31 05:31:18 +0000

    1st_variables

---
+++ .devcontainer/basics/variables1.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "pritam7096p@gmail.com"
+var accountPassword = "123456"
+accountCity = "kolhapur"
+// let accountState;
+
+accountEmail = "prit2604@gmail.com"
+accountPassword = "0986"
+accountCity = "pune"
+
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
