6b27a9f8d6416069880e51bb68672215616e2d21
Pritam Patil <pritam7096p@gmail.com>
2026-04-01 11:39:09 +0000

    conversion

---
+++ .devcontainer/basics/conversionOperation.js
@@ -0,0 +1,17 @@
+
+let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let IsLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(IsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNunber);
