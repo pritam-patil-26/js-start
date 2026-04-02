26106a919bf312d846856eb2fd5c18b93c8a5c76
Pritam Patil <pritam7096p@gmail.com>
2026-04-02 13:28:55 +0000

    operations

---
+++ .devcontainer/basics/conversionOperation1.js
@@ -0,0 +1,22 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+let str1 = "pritam_"
+let str2 = "patil"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 3);
+// console.log(2 + "3");
+// console.log("4" + 1 + 3);
+// console.log(1 + 5 + "4");
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter ++
+console.log(gameCounter);
