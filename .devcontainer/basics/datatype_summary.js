34cac3c3903f23983a0ea44d1077f095665c6412
Pritam Patil <pritam7096p@gmail.com>
2026-04-04 17:58:07 +0000

    summaryofDataType

---
+++ .devcontainer/basics/datatype_summary.js
@@ -0,0 +1,26 @@
+//primitive
+
+//7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 74129652368623154n
+
+//reference(Non primitive)
+
+//Array, Objects, functions
+const heros = ["shaktiman","naagraj","doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+console.log(myObj)
