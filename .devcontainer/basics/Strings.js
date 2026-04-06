65f1b883f2d8ab5096a11d6e86c9e685be28651a
Pritam Patil <pritam7096p@gmail.com>
2026-04-06 14:18:06 +0000

    strings

--- .devcontainer/basics/Strings.js
+++ .devcontainer/basics/Strings.js
@@ -13,4 +13,27 @@ let userOne = {
 let userTwo = userOne
 //console.log(userOne);           //email : "user@gmail.com", upi : "user@ybl"
 userTwo.email = "prit@gmail.com"
-console.log(userTwo.email);      //prit@email.com
+//console.log(userTwo.email);      //prit@email.com
+
+
+//STRINGS
+
+const name = "pritam"
+const age = 21
+// console.log(name + age);
+// console.log(name + age + "_value");
+//console.log(`hello my name is ${name} and my age is ${age}`);        //hello my name is pritam and my age is 21
+const gameName = new String (`pritam`)
+// console.log(gameName[0]);           //p
+// console.log(gameName.__proto__);      //{}
+
+console.log(gameName.length);    //6
+console.log(gameName.toUpperCase());     //PRITAM
+console.log(gameName.charAt(2));      //i
+console.log(gameName.indexOf('t'));       //3
+
+const newString = gameName.substring(0,4)       //prit
+console.log(newString);
+const anotherString = gameName.slice(0,4)
+console.log(anotherString);         //prit
+
