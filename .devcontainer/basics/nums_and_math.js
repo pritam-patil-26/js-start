f8a12eaf57a294139d875376cfe1b0534260c8eb
Pritam Patil <pritam7096p@gmail.com>
2026-04-07 15:37:13 +0000

    precision

--- .devcontainer/basics/nums_and_math.js
+++ .devcontainer/basics/nums_and_math.js
@@ -1,8 +1,15 @@
 const score = 400
-console.log(score);
+console.log(score);                //400
 
 const balance = new Number(100)
-console.log(balance);
+console.log(balance);                 //[Number: 100]
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+console.log(balance.toString().length);          //3
+console.log(balance.toFixed(2));                 //100.00
+
+const otherNumber = 23.8976
+console.log(otherNumber.toPrecision(3));             //23.9   // Must be in the range 1 - 21, inclusive.
+
+
+const number = 123.654
+console.log(number.toPrecision(3));               //124
