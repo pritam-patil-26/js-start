35fadb3d104836d13db9f914a1ea48e42d0bbb47
Pritam Patil <pritam7096p@gmail.com>
2026-04-09 13:49:58 +0000

    array

---
+++ .devcontainer/02_basics/Array.js
@@ -0,0 +1,30 @@
+const myarr = [0,1,2,3,4,5]
+// console.log(myarr);
+// console.log(myarr[1]);
+
+// Array methods
+// myarr.push(7)
+// myarr.push(6)
+// console.log(myarr);
+// myarr.pop()
+// console.log(myarr)
+// 
+// myarr.unshift(9)
+// console.log(myarr);
+
+// myarr.shift()
+// console.log(myarr);
+
+
+// console.log(myarr.includes(4));
+// console.log(myarr.indexOf(5));
+
+// const newarr = myarr.join()
+// console.log(myarr);
+// console.log(newarr);
+
+// console.log(typeof(newarr));
+
+console.log("A" , myarr);
+const mya1 = slice(1,3)
+log
