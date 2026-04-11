c28fc2e139c394d111535c0ac1f379b96f1f0d54
Pritam Patil <pritam7096p@gmail.com>
2026-04-11 17:33:38 +0000

    objects

---
+++ .devcontainer/02_basics/objects.js
@@ -0,0 +1,11 @@
+const JsUser ={
+    name: "Pritam",
+    age: 21,
+    location: "kolhapur",
+    email: "abc@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "Saturday"]
+}
+
+// console.log(JsUser.email);
+console.log(JsUser["email"]);
