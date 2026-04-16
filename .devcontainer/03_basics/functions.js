5f11f2a6413a3abd0e6945846f8770a0d3fa00ff
Pritam Patil <pritam7096p@gmail.com>
2026-04-16 18:51:46 +0000

    functions

--- .devcontainer/03_basics/functions.js
+++ .devcontainer/03_basics/functions.js
@@ -20,7 +20,7 @@ function addTwoNumbers (number1,number2) {
     return result
 }
 const result = addTwoNumbers(3,5)
-console.log("result:",result);       //result: 8
+// console.log("result:",result);       //result: 8
 
 function loginUserMessage(username = "sam"){
     if(username === undefined){
@@ -31,3 +31,31 @@ function loginUserMessage(username = "sam"){
 }
 // console.log(loginUserMessage("pritam"))    //pritam just logged in
 // console.log(loginUserMessage());   //sam just logged in
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200,300,600))
+
+const user = {
+   username: "prit",
+   price: 2000
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 333
+})
+
+const myNewArray = [200,400,100,600]
+ function returnsecondvalue(getArray){
+    return getArray[1]
+ }
+console.log(returnsecondvalue(myNewArray));
+console.log(returnsecondvalue([200,400,100,600]));
+
+ 
