b7bbb3ca4faaaa0256fa514a418ba6161c26f591
Pritam Patil <pritam7096p@gmail.com>
2026-04-16 18:00:19 +0000

    functions

--- .devcontainer/03_basics/functions.js
+++ .devcontainer/03_basics/functions.js
@@ -20,14 +20,14 @@ function addTwoNumbers (number1,number2) {
     return result
 }
 const result = addTwoNumbers(3,5)
-// console.log("result:",result);       //result: 8
+console.log("result:",result);       //result: 8
 
 function loginUserMessage(username = "sam"){
     if(username === undefined){
-        console.log("please enter a username");
+        // console.log("please enter a username");
         return
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("pritam"))    //pritam just logged in
-console.log(loginUserMessage());   //sam just logged in
+// console.log(loginUserMessage("pritam"))    //pritam just logged in
+// console.log(loginUserMessage());   //sam just logged in
